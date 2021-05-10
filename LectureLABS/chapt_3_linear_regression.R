f9c819ef44a095d6caf3e1aa8c4179f0b8f9b1ac
JacobHDahl <jacob.h.dahl@hotmail.no>
2021-05-10 14:00:08 +0200

    first commit

---
+++ LectureLABS/chapt_3_linear_regression.R
@@ -0,0 +1,39 @@
+#rm(list=ls())
+library(MASS)
+library(ISLR)
+
+
+##Single linear regression
+plot(medv ~ lstat,Boston)
+fit1 = lm(medv~lstat,data=Boston)
+abline(fit1,col="red") #plots a line of fit1
+confint(fit1) #confidence interval
+predict(fit1,data.frame(lstat=c(5,10,15)),interval="confidence")
+
+##Multiple linear regression
+fit2 = lm(medv~lstat+age,data=Boston)
+summary(fit2)
+fit3 = lm(medv~.,Boston) #fits a linear model(lm) for all (.) variables available against medv
+summary(fit3)
+par(mfrow=c(2,2)) #configures plot to 2x2 matrix of plots
+plot(fit3)
+fit4=update(fit3,~.-age-indus) #fit against all available -age and indus
+summary(fit4)
+
+##Nonlinear terms and interactions
+fit5=lm(medv~lstat*age,Boston) #fits an linear model with an interaction AND the individual values (hierarchy principle)
+summary(fit5)
+fit6=lm(medv~lstat + I(lstat^2),Boston); summary(fit6) #adds a quadratic term, need I() to protect against the ^2 which is interprated differently in R (:)
+attach(Boston) #makes the named variables in Boston available
+par(mfrow=c(1,1)) #configures plot window with 1 plot
+plot(medv~lstat)
+points(lstat,fitted(fit6),col="red",pch=20) #plots each value from lstat against the fitted values of fit6
+fit7=lm(medv~poly(lstat,4)) #linear model with 4th power term
+fit8=lm(medv~poly(lstat,2)) #better way of fitting polynomial than the I(^2) thingy
+points(lstat,fitted(fit7),col="blue",pch=20)
+points(lstat,fitted(fit8),col="green",pch=10)
+plot(1:20,1:20,pch=1:20,cex=2) #20 availabelplotting characters, cex=2 doubles size of character
+
+##Qualitative predictors
+
+
